b0f7f8b35dc1150a32ff940e6663b6e0023c38da
Etxebe <mikelgartzia21@gmail.com>
2023-10-03 15:37:56 +0200

    [NEW] creado Heafer.js en carpeta components y sus estilos

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar, ScrollView } from 'expo-status-bar';
+import Header from './components/Header';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () => {
+  return(
+    <>
+    <StatusBar
+      backgroundColor= "#FFFFFF"
+      barStyle= "dark-content"
+    />
+    <ScrollView>
+        <Header/>
+    </ScrollView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
