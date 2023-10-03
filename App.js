65dd8858e6a6c2ec27dce862925f29ca2cc9f03e
Etxebe <mikelgartzia21@gmail.com>
2023-10-03 16:14:59 +0200

    [NEW] instalado paquete para navegacion tabs, importadas dependencias y paquete de iconos y configurados en app.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar, ScrollView } from 'expo-status-bar';
+import { NavigationContainer} from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 import Header from './components/Header';
 import Home from './screens/Home';
 import Friends from './screens/Home';
@@ -9,6 +12,7 @@ import Profile from './screens/Profile';
 import Notification from './screens/Notification';
 import Menu from './screens/Menu';
 
+const Tab = createMaterialTopTabNavigator()
 
 const App = () => {
   return(
@@ -23,5 +27,39 @@ const App = () => {
     </>
   );
 }
+
+<NavigationContainer>
+  <Tab.Navigator
+    swipeEnabled = 'true'
+    tabBarOptions={{
+      showIcon: true,
+      showLabel: false,
+      activeTintColor: '#3a86e9',
+      inactiveTintColor: '#9F9F9F',
+
+    }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({getFocusedRouteNameFromRoute, color, size}) =>{
+        let iconName;
+        if(route.name === 'Home') iconName = 'home'
+        else if(route.name === 'Friends') iconName = 'account-multiple-outline'
+        else if(route.name === 'Groups') iconName = 'account-group'
+        else if(route.name === 'Profile') iconName = 'account-circle-outline'
+        else if(route.name === 'Notification') iconName = 'bell-outline'
+        else if(route.name === 'Menu') iconName = 'menu'
+        return <MaterialCommunityIcons name = {iconName} size={26} color={color} />
+    
+        },
+    })}
+  >
+  <Tab.Screen name = "Home" component={Home} />
+  <Tab.Screen name = "Friends" component={Friends} />
+  <Tab.Screen name = "Groups" component={Groups} />
+  <Tab.Screen name = "Profile" component={Profile} />
+  <Tab.Screen name = "Notifications" component={Notification} />
+  <Tab.Screen name = "Menu" component={Menu} />
+  </Tab.Navigator>
+</NavigationContainer>
+
 
 export default App
